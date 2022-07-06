d667e5abf5dc93bd0b6d8bebd275febda9b3a46a
kevin kinyua <51110607+sirkeygun@users.noreply.github.com>
2022-07-06 22:56:54 +0300

    added layout files

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
 const express = require('express')
 const app = express()
 
-
+app.use(express.static('public'))
 
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-    console.log('landing page')
-    res.render('index')
-    
-    })
+
 
 const userRouter = require('./routes/user')
 
 app.use('/user', userRouter)
 
-app.listen(3000);
+app.listen(3001);
