d7f998f62fe9568786ecd37ddee014af9abf6b35
kevin kinyua <51110607+sirkeygun@users.noreply.github.com>
2022-07-08 22:38:38 +0300

    css

--- main.js
+++ main.js
@@ -7,15 +7,13 @@ app.set('view engine', 'ejs');
 // use res.render to load up an ejs view file
 
 // index page
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
   res.render('pages/index');
 });
 
 // about page
-app.get('/about', function(req, res) {
-  res.render('pages/about');
-});
+
 
 app.listen(8080);
 console.log('Server is listening on port 8080');
